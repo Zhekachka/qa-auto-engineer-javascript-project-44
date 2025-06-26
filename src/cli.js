ecf6220411405fbc47430cc7caee2aa78a28b89c
ekokareva <ekokareva@ozon.ru>
2025-06-26 11:42:44 +0300

    comment

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,4 @@ const askForTheName = () => {
   const name = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${name}!`)
 }
-export default askForTheName
+export default askForTheName //comment
